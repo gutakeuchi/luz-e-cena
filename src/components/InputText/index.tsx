ca9d4c9e919fcb9758e72c8ce96048625b7bd0c4
Gustavo Takeuchi <gustavo.takeuchi87@gmail.com>
2025-12-04 19:01:04 -0300

    atualizando coisas

---
+++ src/components/InputText/index.tsx
@@ -0,0 +1,11 @@
+import styles from './InputText.module.css'
+
+const InputText = ({ ...props }: React.InputHTMLAttributes<HTMLInputElement>) => {
+    return (
+        <input {...props} className={styles.input}
+
+        />
+    )
+}
+
+export default InputText
